0184a399a111313b607aeb99f70cd77d2ce02d2b
Raúl Fernández Fernández <raulparri71@gmail.com>
2022-06-05 14:11:35 +0200

    fix: avoid log entries created from user input

--- internal/platform/server/http/middleware/logging/logging.go
+++ internal/platform/server/http/middleware/logging/logging.go
@@ -13,12 +13,6 @@ func Middleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Start timer
 		start := time.Now()
-		// Request URL path
-		path := c.Request.URL.Path
-
-		if c.Request.URL.RawQuery != "" {
-			path = path + "?" + c.Request.URL.RawQuery
-		}
 
 		// Process request
 		c.Next()
@@ -29,6 +23,7 @@ func Middleware() gin.HandlerFunc {
 		clientIP := c.ClientIP()
 		method := c.Request.Method
 		statusCode := c.Writer.Status()
+		path := c.Request.URL.Path
 
 		fmt.Printf("[HTTP] %v | %3d | %13v | %15s | %-7s %#v\n",
 			timestamp.Format("2006/01/02 - 15:04:05"),
