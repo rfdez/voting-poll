3a66eac1a6e35d7d1d122d92d584593ea3fb6280
Raúl Fernández Fernández <raulparri71@gmail.com>
2022-06-05 17:30:07 +0200

    chore: update app entrypoint and go modules

--- cmd/poll/main.go
+++ cmd/poll/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
 	"log"
 	"time"
 
 	"github.com/kelseyhightower/envconfig"
+	_ "github.com/lib/pq"
+	"github.com/rfdez/voting-poll/internal/creator"
 	"github.com/rfdez/voting-poll/internal/platform/bus/inmemory"
 	"github.com/rfdez/voting-poll/internal/platform/server/http"
+	"github.com/rfdez/voting-poll/internal/platform/storage/postgresql"
 )
 
 func main() {
@@ -17,9 +22,29 @@ func main() {
 		log.Fatal(err)
 	}
 
+	psqlURI := fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?%s", cfg.DbUser, cfg.DbPass, cfg.DbHost, cfg.DbPort, cfg.DbName, cfg.DbParams)
+	db, err := sql.Open("postgres", psqlURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	var (
 		commandBus = inmemory.NewCommandBus()
 	)
+
+	var (
+		pollRepository = postgresql.NewPollRepository(db, cfg.DbTimeout)
+	)
+
+	var (
+		creatorService = creator.NewService(pollRepository)
+	)
+
+	var (
+		createPollCommandHandler = creator.NewPollCommandHandler(creatorService)
+	)
+
+	commandBus.Register(creator.PollCommandType, createPollCommandHandler)
 
 	ctx, httpSrv := http.NewServer(context.Background(), cfg.HttpHost, cfg.HttpPort, cfg.ShutdownTimeout, commandBus)
 	if err := httpSrv.Run(ctx); err != nil {
@@ -32,4 +57,12 @@ type config struct {
 	HttpHost        string        `default:""`
 	HttpPort        uint          `default:"8080"`
 	ShutdownTimeout time.Duration `default:"10s"`
+	// Database configuration
+	DbUser    string        `default:"poll"`
+	DbPass    string        `default:"poll"`
+	DbHost    string        `default:"localhost"`
+	DbPort    uint          `default:"5432"`
+	DbName    string        `default:"voting_poll"`
+	DbParams  string        `default:"sslmode=disable"`
+	DbTimeout time.Duration `default:"5s"`
 }
