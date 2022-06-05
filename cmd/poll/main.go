b0d8c0b90e62d2aff9bc8a88b97a680d796ac8fa
Raúl Fernández Fernández <raulparri71@gmail.com>
2022-06-05 13:30:46 +0200

    feat(http): add server and ping endpoint. Use middlewares

--- cmd/poll/main.go
+++ cmd/poll/main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"time"
 
-	"github.com/google/go-cmp/cmp"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/rfdez/voting-poll/internal/platform/server/http"
 )
 
 func main() {
-	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	var cfg config
+	err := envconfig.Process("POLL", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, httpSrv := http.NewServer(context.Background(), cfg.HttpHost, cfg.HttpPort, cfg.ShutdownTimeout)
+	if err := httpSrv.Run(ctx); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type config struct {
+	// Http Server configuration
+	HttpHost        string        `default:""`
+	HttpPort        uint          `default:"8080"`
+	ShutdownTimeout time.Duration `default:"10s"`
 }
---
+++ internal/platform/server/http/server.go
@@ -0,0 +1,81 @@
+package http
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rfdez/voting-poll/internal/platform/server/http/handler/health"
+	"github.com/rfdez/voting-poll/internal/platform/server/http/middleware/logging"
+	"github.com/rfdez/voting-poll/internal/platform/server/http/middleware/recovery"
+)
+
+type Server struct {
+	httpAddr string
+	engine   *gin.Engine
+
+	shutdownTimeout time.Duration
+}
+
+func NewServer(ctx context.Context, host string, port uint, shutdownTimeout time.Duration) (context.Context, Server) {
+	gin.SetMode(gin.ReleaseMode)
+
+	srv := Server{
+		httpAddr: fmt.Sprintf("%s:%d", host, port),
+		engine:   gin.New(),
+
+		shutdownTimeout: shutdownTimeout,
+	}
+
+	srv.registerRoutes()
+	return serverContext(ctx), srv
+}
+
+func (s *Server) registerRoutes() {
+	// Register middleware
+	s.engine.Use(recovery.Middleware(), logging.Middleware())
+
+	// Register routes
+	s.engine.GET("/ping", health.PingHandler())
+}
+
+func (s *Server) Run(ctx context.Context) error {
+	log.Printf("HTTP server running on %s", s.httpAddr)
+
+	srv := &http.Server{
+		Addr:    s.httpAddr,
+		Handler: s.engine,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal("HTTP server error: ", err)
+		}
+	}()
+
+	<-ctx.Done()
+	ctxShutDown, cancel := context.WithTimeout(context.Background(), s.shutdownTimeout)
+	defer cancel()
+
+	log.Println("Server shutting down...")
+
+	return srv.Shutdown(ctxShutDown)
+}
+
+func serverContext(ctx context.Context) context.Context {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	ctx, cancel := context.WithCancel(ctx)
+	go func() {
+		<-c
+		cancel()
+	}()
+
+	return ctx
+}
